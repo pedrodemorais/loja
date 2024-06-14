553d8a57b2691ab1ed7d9d87bc2861628cdce4b7
Pedro Morais <re045163@qintess.com>
2024-06-14 16:23:23 -0300

    Criado classe model/CategoriaProduto.java

---
+++ src/main/java/com/example/demo/com/example/demo/model/CategoriaProduto.java
@@ -0,0 +1,51 @@
+package com.example.demo.com.example.demo.model;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name="categoria_produto")
+@SequenceGenerator(name="seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1,initialValue = 1)
+public class CategoriaProduto implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+    private Long id;
+
+    @Column(name ="nome_desc", nullable = false)
+    private String nomeDesc;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CategoriaProduto)) return false;
+        CategoriaProduto that = (CategoriaProduto) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/example/demo/com/example/demo/model/MarcaProduto.java
+++ src/main/java/com/example/demo/com/example/demo/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package com.example.demo.com.example.demo.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name="marca_produto")
@@ -12,6 +13,7 @@ public class MarcaProduto implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
     private Long id;
+    @Column(nullable = false)
     private String nomeDesc;
 
     public Long getId() {
@@ -28,5 +30,22 @@ public class MarcaProduto implements Serializable {
 
     public void setNomeDesc(String nomeDesc) {
         this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof MarcaProduto)) return false;
+        MarcaProduto that = (MarcaProduto) o;
+        return id.equals(that.id) && nomeDesc.equals(that.nomeDesc);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime=31;
+        int result=1;
+        result = prime * result + (((id == null))?0:id.hashCode());
+        return result;
     }
 }
