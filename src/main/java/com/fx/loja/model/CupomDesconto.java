dd08e2b50da8badde96bd2187c4d8678c56d2669
Pedro Morais <re045163@qintess.com>
2024-06-18 11:45:30 -0300

    Criado as classes CupomDesconto ImagemProduto NotaFiscalCompra NotaItemProduto e Produto

---
+++ src/main/java/com/fx/loja/model/CupomDesconto.java
@@ -0,0 +1,79 @@
+package com.fx.loja.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="cupom_desconto")
+@SequenceGenerator(name="seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+    private Long id;
+
+    private String CodigoDescricao;
+    private BigDecimal valorRealDesconto;
+    private BigDecimal valorPercentualDesconto;
+    @Temporal(TemporalType.DATE)
+    private Date dataValidadeCupom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoDescricao() {
+        return CodigoDescricao;
+    }
+
+    public void setCodigoDescricao(String codigoDescricao) {
+        CodigoDescricao = codigoDescricao;
+    }
+
+    public BigDecimal getValorRealDesconto() {
+        return valorRealDesconto;
+    }
+
+    public void setValorRealDesconto(BigDecimal valorRealDesconto) {
+        this.valorRealDesconto = valorRealDesconto;
+    }
+
+    public BigDecimal getValorPercentualDesconto() {
+        return valorPercentualDesconto;
+    }
+
+    public void setValorPercentualDesconto(BigDecimal valorPercentualDesconto) {
+        this.valorPercentualDesconto = valorPercentualDesconto;
+    }
+
+    public Date getDataValidadeCupom() {
+        return dataValidadeCupom;
+    }
+
+    public void setDataValidadeCupom(Date dataValidadeCupom) {
+        this.dataValidadeCupom = dataValidadeCupom;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CupomDesconto)) return false;
+        CupomDesconto that = (CupomDesconto) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/fx/loja/model/NotaFiscalCompra.java
@@ -0,0 +1,128 @@
+package com.fx.loja.model;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="nota_fiscal_compra")
+@SequenceGenerator(name="seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1, initialValue = 1)
+public class NotaFiscalCompra {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNota;
+    private String serieNota;
+    private String descricaoObservacao;
+    private BigDecimal valorTotal = BigDecimal.ZERO;
+    private BigDecimal valorDesconto = BigDecimal.ZERO;
+    private BigDecimal valorIcms = BigDecimal.ZERO;
+    @Temporal(TemporalType.DATE)
+    private Date DataCompra;
+
+    @ManyToOne(targetEntity = Pessoa.class)//muitas notas de compra para uma pessoa
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value=ConstraintMode.CONSTRAINT,name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne//muitas notas para uma conta pagar
+    @JoinColumn(name = "contas_pagar_id", nullable = false, foreignKey = @ForeignKey(value=ConstraintMode.CONSTRAINT,name = "contas_pagar_fk"))
+    private ContasPagar contasPagar;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroNota() {
+        return numeroNota;
+    }
+
+    public void setNumeroNota(String numeroNota) {
+        this.numeroNota = numeroNota;
+    }
+
+    public String getSerieNota() {
+        return serieNota;
+    }
+
+    public void setSerieNota(String serieNota) {
+        this.serieNota = serieNota;
+    }
+
+    public String getDescricaoObservacao() {
+        return descricaoObservacao;
+    }
+
+    public void setDescricaoObservacao(String descricaoObservacao) {
+        this.descricaoObservacao = descricaoObservacao;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public BigDecimal getValorIcms() {
+        return valorIcms;
+    }
+
+    public void setValorIcms(BigDecimal valorIcms) {
+        this.valorIcms = valorIcms;
+    }
+
+    public Date getDataCompra() {
+        return DataCompra;
+    }
+
+    public void setDataCompra(Date dataCompra) {
+        DataCompra = dataCompra;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public ContasPagar getContasPagar() {
+        return contasPagar;
+    }
+
+    public void setContasPagar(ContasPagar contasPagar) {
+        this.contasPagar = contasPagar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NotaFiscalCompra)) return false;
+        NotaFiscalCompra that = (NotaFiscalCompra) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
